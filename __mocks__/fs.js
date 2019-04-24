f1b16acfbfc48daebb6bc28a79d96a8d9aa98af9
Michael Rose <michael.rose@tophatmonocle.com>
2019-04-24 14:30:05 -0400

    Move fs mock back to mock file

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,4 +1,21 @@
-import { fs } from 'memfs'
+import path from 'path'
+import { fs, vol } from 'memfs'
+
+const packagesRoot = path.resolve(process.cwd(), 'packages')
+
+vol.fromJSON(
+    {
+        'package1/package.json': JSON.stringify({
+            "description": "A collection of common visual components.",
+            "felibPackageGroup": "components",
+            "name": "@thm/fe-common-components",
+            "version": "0.1.0",
+          }),
+        'package2/package.json': '{}',
+        'package3/package.json': '{}',
+    },
+    packagesRoot
+)
 
 // This needs to be module.exports, not export default, in order to mock fs properly
 module.exports = fs
