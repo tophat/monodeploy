262ca9eba057c5d8ddb51ce2d2056ab1fa3a7461
Michael Rose <michael_rose@outlook.com>
2020-02-01 22:58:54 -0500

    Start rewrite

--- monorepo/packages/test-package-2/CHANGELOG.md
+++ monorepo/packages/test-package-2/CHANGELOG.md
@@ -11,6 +11,14 @@ See [Conventional Commits](https://conventionalcommits.org) for commit guideline
 
 
 
+## [0.1.1](https://github.com/tophat/monodeploy/compare/@msrose/test-package-2@1.2.4...@msrose/test-package-2@0.1.1) (2020-02-02)
+
+**Note:** Version bump only for package @msrose/test-package-2
+
+
+
+
+
 ## 1.2.4 (2020-02-02)
 
 **Note:** Version bump only for package @msrose/test-package-2
---
+++ src/rewrite.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const path = require('path')
+
+const { getPackages } = require('@lerna/project')
+const latestVersion = require('latest-version')
+const PackageGraph = require('@lerna/package-graph')
+const collectUpdates = require('@lerna/collect-updates')
+const lernaVersion = require('@lerna/version')
+const lernaPublish = require('@lerna/publish')
+
+async function run() {
+    const packages = await getPackages(process.cwd())
+
+    for (const pkg of packages) {
+        try {
+            const version = await latestVersion(pkg.name)
+            pkg.version = version
+        } catch (e) {
+            pkg.version = '0.1.0'
+        }
+    }
+
+    const graph = new PackageGraph(packages)
+    const changedPackages = collectUpdates(
+        packages,
+        graph,
+        { cwd: process.cwd() },
+        { ignoreChanges: [] },
+    )
+
+    // TODO: use package graph and localDependencies here
+    changedPackages.forEach(({ pkg }) => {
+        Object.keys(pkg.dependencies || {}).forEach(dependency => {
+            const siblingPackage = packages.find(pkg => pkg.name === dependency)
+            if (siblingPackage) {
+                pkg.dependencies[dependency] = `^${siblingPackage.version}`
+            }
+        })
+    })
+
+    for (const { pkg } of changedPackages) {
+        fs.writeFileSync(
+            path.join(pkg.location, 'package.json'),
+            JSON.stringify(pkg, null, 2),
+        )
+    }
+
+    await lernaVersion({
+        amend: true,
+        yes: true,
+        conventionalCommits: true,
+    })
+
+    await lernaPublish({
+        bump: 'from-git',
+    })
+}
+
+run()
