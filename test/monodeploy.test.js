8abe36a16ff7de50615e9df5f47b9836b7495128
Michael Rose <michael_rose@outlook.com>
2020-09-15 15:46:06 -0400

    chore: Put test cleanup in single function (#153)
    * Put test cleanup in single function
    
    * Add semantic config

--- test/monodeploy.test.js
+++ test/monodeploy.test.js
@@ -170,48 +170,63 @@ describe('monodeploy', () => {
         return _monodeploy(options, monorepo.getPath(), resources)
     }
 
+    // Use a weird context manager style API to make sure we clean up the
+    // monorepo at the end.  Using afterEach to clean up doesn't work in this
+    // case for some reason, maybe because of some way jest handles async tests
+    const withMonorepo = monorepo => {
+        return {
+            async do(fn) {
+                await fn(monorepo)
+                await monorepo.delete()
+            },
+        }
+    }
+
     it('publishes packages for the first time', async () => {
         const monorepo = await createMonorepo({
             packages: { 'package-0': [], 'package-1': [], 'package-2': [] },
         })
-        await monodeploy(monorepo)
-        await expect('package-0').toHaveVersion('1.0.1')
-        await expect('package-1').toHaveVersion('1.0.1')
-        await expect('package-2').toHaveVersion('1.0.1')
-        await monorepo.delete()
+        await withMonorepo(monorepo).do(async () => {
+            await monodeploy(monorepo)
+            await expect('package-0').toHaveVersion('1.0.1')
+            await expect('package-1').toHaveVersion('1.0.1')
+            await expect('package-2').toHaveVersion('1.0.1')
+        })
     })
 
     it('does not bump packages if they have not been changed', async () => {
         const monorepo = await createMonorepo({
             packages: { 'package-0': [], 'package-1': [], 'package-2': [] },
         })
-        await monodeploy(monorepo)
-        await expect('package-0').toHaveVersion('1.0.1')
-        await expect('package-1').toHaveVersion('1.0.1')
-        await expect('package-2').toHaveVersion('1.0.1')
-        await monodeploy(monorepo)
-        await expect('package-0').toHaveVersion('1.0.1')
-        await expect('package-1').toHaveVersion('1.0.1')
-        await expect('package-2').toHaveVersion('1.0.1')
-        await monorepo.delete()
+        await withMonorepo(monorepo).do(async () => {
+            await monodeploy(monorepo)
+            await expect('package-0').toHaveVersion('1.0.1')
+            await expect('package-1').toHaveVersion('1.0.1')
+            await expect('package-2').toHaveVersion('1.0.1')
+            await monodeploy(monorepo)
+            await expect('package-0').toHaveVersion('1.0.1')
+            await expect('package-1').toHaveVersion('1.0.1')
+            await expect('package-2').toHaveVersion('1.0.1')
+        })
     })
 
     it('bumps the version of changed packages', async () => {
         const monorepo = await createMonorepo({
             packages: { 'package-0': [], 'package-1': [], 'package-2': [] },
         })
-        await monodeploy(monorepo)
-        await monorepo.addFileToPackage(
-            'package-0',
-            'newFile.js',
-            'console.log("hi")',
-        )
-        await monorepo.commitChanges({ message: 'Add newFile' })
-        await monodeploy(monorepo)
-        await expect('package-0').toHaveVersion('1.0.2')
-        await expect('package-1').toHaveVersion('1.0.1')
-        await expect('package-2').toHaveVersion('1.0.1')
-        await monorepo.delete()
+        await withMonorepo(monorepo).do(async () => {
+            await monodeploy(monorepo)
+            await monorepo.addFileToPackage(
+                'package-0',
+                'newFile.js',
+                'console.log("hi")',
+            )
+            await monorepo.commitChanges({ message: 'Add newFile' })
+            await monodeploy(monorepo)
+            await expect('package-0').toHaveVersion('1.0.2')
+            await expect('package-1').toHaveVersion('1.0.1')
+            await expect('package-2').toHaveVersion('1.0.1')
+        })
     })
 
     it('bumps dependent packages', async () => {
@@ -222,69 +237,73 @@ describe('monodeploy', () => {
                 'package-2': [],
             },
         })
-        await monodeploy(monorepo)
-        await monorepo.addFileToPackage(
-            'package-1',
-            'newFile.js',
-            'console.log("hi")',
-        )
-        await monorepo.commitChanges({ message: 'Add newFile' })
-        await monodeploy(monorepo)
-        await expect('package-0').toHaveVersion('1.0.2')
-        await expect('package-1').toHaveVersion('1.0.2')
-        await expect('package-2').toHaveVersion('1.0.1')
-        await monorepo.delete()
+        await withMonorepo(monorepo).do(async () => {
+            await monodeploy(monorepo)
+            await monorepo.addFileToPackage(
+                'package-1',
+                'newFile.js',
+                'console.log("hi")',
+            )
+            await monorepo.commitChanges({ message: 'Add newFile' })
+            await monodeploy(monorepo)
+            await expect('package-0').toHaveVersion('1.0.2')
+            await expect('package-1').toHaveVersion('1.0.2')
+            await expect('package-2').toHaveVersion('1.0.1')
+        })
     })
 
     it('writes a latest versions file if name is given', async () => {
         const monorepo = await createMonorepo({
             packages: { 'package-0': [], 'package-1': [], 'package-2': [] },
         })
-        await monodeploy(monorepo, {
-            latestVersionsFile: 'latest-versions.json',
+        await withMonorepo(monorepo).do(async () => {
+            await monodeploy(monorepo, {
+                latestVersionsFile: 'latest-versions.json',
+            })
+            const latestVersionsContents = await fs.readFile(
+                path.join(monorepo.getPath(), 'latest-versions.json'),
+                'utf-8',
+            )
+            expect(latestVersionsContents).toMatchSnapshot()
         })
-        const latestVersionsContents = await fs.readFile(
-            path.join(monorepo.getPath(), 'latest-versions.json'),
-            'utf-8',
-        )
-        expect(latestVersionsContents).toMatchSnapshot()
-        await monorepo.delete()
     })
 
     it('bumps the minor version for features', async () => {
         const monorepo = await createMonorepo({
             packages: { 'package-0': [], 'package-1': [], 'package-2': [] },
         })
-        await monodeploy(monorepo)
-        await monorepo.addFileToPackage(
-            'package-1',
-            'newFile.js',
-            'console.log("hi")',
-        )
-        await monorepo.commitChanges({ message: 'feat: Add newFile' })
-        await monodeploy(monorepo)
-        await expect('package-0').toHaveVersion('1.0.1')
-        await expect('package-1').toHaveVersion('1.1.0')
-        await expect('package-2').toHaveVersion('1.0.1')
-        await monorepo.delete()
+        await withMonorepo(monorepo).do(async () => {
+            await monodeploy(monorepo)
+            await monorepo.addFileToPackage(
+                'package-1',
+                'newFile.js',
+                'console.log("hi")',
+            )
+            await monorepo.commitChanges({ message: 'feat: Add newFile' })
+            await monodeploy(monorepo)
+            await expect('package-0').toHaveVersion('1.0.1')
+            await expect('package-1').toHaveVersion('1.1.0')
+            await expect('package-2').toHaveVersion('1.0.1')
+        })
     })
 
     it('bumps the major version for breaking changes', async () => {
         const monorepo = await createMonorepo({
             packages: { 'package-0': [], 'package-1': [], 'package-2': [] },
         })
-        await monodeploy(monorepo)
-        await monorepo.addFileToPackage(
-            'package-1',
-            'newFile.js',
-            'console.log("hi")',
-        )
-        await monorepo.commitChanges({
-            message: 'feat: Add newFile\n\nBREAKING CHANGE: Yeet the yeet',
+        await withMonorepo(monorepo).do(async () => {
+            await monodeploy(monorepo)
+            await monorepo.addFileToPackage(
+                'package-1',
+                'newFile.js',
+                'console.log("hi")',
+            )
+            await monorepo.commitChanges({
+                message: 'feat: Add newFile\n\nBREAKING CHANGE: Yeet the yeet',
+            })
+            await monodeploy(monorepo)
+            await expect('package-1').toHaveVersion('2.0.0')
         })
-        await monodeploy(monorepo)
-        await expect('package-1').toHaveVersion('2.0.0')
-        await monorepo.delete()
     })
 
     it('updates the package.json files of the published packages', async () => {
@@ -295,17 +314,18 @@ describe('monodeploy', () => {
                 'package-2': [],
             },
         })
-        await monodeploy(monorepo)
-        await monorepo.addFileToPackage(
-            'package-1',
-            'newFile.js',
-            'console.log("hi")',
-        )
-        await monorepo.commitChanges({ message: 'Add newFile' })
-        await monodeploy(monorepo)
-        expect(await monorepo.getPackageJSON('package-0')).toMatchSnapshot()
-        expect(await monorepo.getPackageJSON('package-1')).toMatchSnapshot()
-        expect(await monorepo.getPackageJSON('package-2')).toMatchSnapshot()
-        await monorepo.delete()
+        await withMonorepo(monorepo).do(async () => {
+            await monodeploy(monorepo)
+            await monorepo.addFileToPackage(
+                'package-1',
+                'newFile.js',
+                'console.log("hi")',
+            )
+            await monorepo.commitChanges({ message: 'Add newFile' })
+            await monodeploy(monorepo)
+            expect(await monorepo.getPackageJSON('package-0')).toMatchSnapshot()
+            expect(await monorepo.getPackageJSON('package-1')).toMatchSnapshot()
+            expect(await monorepo.getPackageJSON('package-2')).toMatchSnapshot()
+        })
     })
 })
