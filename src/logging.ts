13e882d4844c6ca9a85e3a97f6e8d26d26a143b9
noahnu <noahnu@gmail.com>
2020-12-29 15:32:20 -0500

    wip: better logging

--- src/logging.ts
+++ src/logging.ts
@@ -22,6 +22,12 @@ const levelToColour = {
     [LOG_LEVELS.ERROR]: chalk.red,
 }
 
+const leftPad = (value: string | number, padding: number): string => {
+    const str = String(value)
+    const whitespace = '0'.repeat(Math.max(padding - str.length, 0))
+    return `${whitespace}${str}`
+}
+
 const getCurrentLogLevel = () => {
     try {
         const envLogLevel = process.env.MONODEPLOY_LOG_LEVEL
@@ -37,13 +43,19 @@ const createLogger = (level: LogLevelType): Logger => (
     ...args: unknown[]
 ): void => {
     if (getCurrentLogLevel() > level) return
-    const timestamp = `[${Date.now()}]`
+    const date = new Date()
+    const timestamp = `[${leftPad(date.getHours(), 2)}:${leftPad(
+        date.getMinutes(),
+        2,
+    )}:${leftPad(date.getSeconds(), 2)}.${leftPad(date.getMilliseconds(), 3)}]`
     const colour = levelToColour[level]
 
-    console.log(
-        chalk.yellow(timestamp),
-        ...args.map((a, index) => (index > 0 ? a : (colour as Formatter)(a))),
-    )
+    console.log(chalk.yellow(timestamp), (colour as Formatter)(args[0]))
+    if (args.length > 1) {
+        for (const arg of args.slice(1)) {
+            console.log(arg)
+        }
+    }
 }
 
 const logger = {
