42a703c8ec2e0ec7d25fb838d02e1f08ae8ba979
Noah <noah.negin-ulster@tophatmonocle.com>
2021-02-13 14:53:50 -0500

    test: io coverage (#247)

--- packages/io/src/stream.test.ts
+++ packages/io/src/stream.test.ts
@@ -1,6 +1,6 @@
 import { Readable } from 'stream'
 
-import { readStream } from './stream'
+import { readStream, readStreamString } from './stream'
 
 describe('Stream to Chunks', () => {
     it('flushes a stream to an array of chunks', async () => {
@@ -10,6 +10,21 @@ describe('Stream to Chunks', () => {
         expect(stream).not.toEqual(original)
 
         const transformed = await readStream(stream)
+        expect(transformed).toEqual(original)
+    })
+})
+
+describe('Stream to String', () => {
+    it('flushes a stream to a string', async () => {
+        const original = 'this is a sentence'
+
+        const stream = Readable.from([
+            original.substring(0, 5),
+            original.substring(5),
+        ])
+        expect(stream).not.toEqual(original)
+
+        const transformed = await readStreamString(stream)
         expect(transformed).toEqual(original)
     })
 })
