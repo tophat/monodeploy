8e3a44e4a42447c88654ecb21e84b0cb83784da4
noahnu <noahnu@gmail.com>
2021-03-06 21:18:45 -0500

    test: more coverage

--- packages/node/src/monodeploy.test.ts
+++ packages/node/src/monodeploy.test.ts
@@ -37,25 +37,17 @@ const mockNPM = npm as jest.Mocked<
 >
 
 const setupExampleMonorepo = async (): Promise<YarnContext> => {
-    const scripts = {
-        prepack: 'touch .prepack.test.tmp',
-        prepare: 'touch .prepare.test.tmp',
-        prepublish: 'touch .prepublish.test.tmp',
-        postpack: 'touch .postpack.test.tmp',
-        postpublish: 'touch .postpublish.test.tmp',
-    }
     const context = await setupMonorepo(
         {
             'pkg-1': {},
             'pkg-2': {},
             'pkg-3': { dependencies: ['pkg-2'] },
-            'pkg-4': { scripts },
-            'pkg-5': { dependencies: ['pkg-4'], scripts },
+            'pkg-4': {},
+            'pkg-5': { dependencies: ['pkg-4'] },
             'pkg-6': {
                 dependencies: ['pkg-3', 'pkg-7'],
-                scripts,
             },
-            'pkg-7': { scripts },
+            'pkg-7': {},
         },
         {
             root: {
@@ -541,24 +533,42 @@ describe('Monodeploy Lifecycle Scripts', () => {
             filename,
         )
 
-    const cleanup = async pkgName => {
-        const pkgDir = path.join(monodeployConfig.cwd, 'packages', pkgName)
-        for (const tmpFile of await fs.readdir(pkgDir)) {
-            const resolvedFile = path.resolve(pkgDir, tmpFile)
-            if (resolvedFile.endsWith('.tmp')) {
-                try {
-                    await fs.unlink(resolvedFile)
-                } catch {}
-            }
-        }
-    }
-
     beforeAll(async () => {
         process.env.MONODEPLOY_LOG_LEVEL = String(LOG_LEVELS.ERROR)
     })
 
     beforeEach(async () => {
-        const context = await setupExampleMonorepo()
+        const scripts = {
+            prepack: 'node -p "process.hrtime.bigint()" > .prepack.test.tmp',
+            prepare: 'node -p "process.hrtime.bigint()" > .prepare.test.tmp',
+            prepublish:
+                'node -p "process.hrtime.bigint()" > .prepublish.test.tmp',
+            postpack: 'node -p "process.hrtime.bigint()" > .postpack.test.tmp',
+            postpublish:
+                'node -p "process.hrtime.bigint()" > .postpublish.test.tmp',
+        }
+        const context = await setupMonorepo(
+            {
+                'pkg-1': {},
+                'pkg-2': {},
+                'pkg-3': { dependencies: ['pkg-2'] },
+                'pkg-4': { scripts },
+                'pkg-5': { dependencies: ['pkg-4'], scripts },
+                'pkg-6': {
+                    dependencies: ['pkg-3', 'pkg-7'],
+                    devDependencies: ['pkg-1'],
+                    scripts,
+                },
+                'pkg-7': { scripts },
+            },
+            {
+                root: {
+                    dependencies: {
+                        '@tophat/conventional-changelog-config': '^0.5.0',
+                    },
+                },
+            },
+        )
         monodeployConfig.cwd = context.project.cwd
     })
 
@@ -576,45 +586,55 @@ describe('Monodeploy Lifecycle Scripts', () => {
 
     it('runs lifecycle scripts for changed workspaces', async () => {
         mockNPM._setTag_('pkg-1', '0.0.1')
-        mockNPM._setTag_('pkg-2', '0.0.1')
+        mockNPM._setTag_('pkg-2', '0.1.1')
         mockNPM._setTag_('pkg-3', '0.0.1')
         mockNPM._setTag_('pkg-4', '0.0.1')
         mockNPM._setTag_('pkg-5', '0.0.1')
+        mockNPM._setTag_('pkg-6', '0.0.1')
         mockGit._commitFiles_('sha1', 'feat: some new feature!', [
             './packages/pkg-4/README.md',
         ])
+        mockGit._commitFiles_('sha1', 'feat: some other feature!', [
+            './packages/pkg-2/README.md',
+        ])
 
-        try {
-            const result = await monodeploy(monodeployConfig)
+        const result = await monodeploy(monodeployConfig)
 
-            // pkg-1 is explicitly updated with minor bump
-            expect(result['pkg-4'].version).toEqual('0.1.0')
-            expect(result['pkg-4'].changelog).toEqual(
-                expect.stringContaining('some new feature'),
-            )
+        // pkg-4 is explicitly updated with minor bump
+        expect(result['pkg-4'].version).toEqual('0.1.0')
+        expect(result['pkg-4'].changelog).toEqual(
+            expect.stringContaining('some new feature'),
+        )
 
-            // pkg-5 depends on pkg-4, so it'll be bumped as a dependant
-            expect(mockGit._getPushedTags_()).toEqual([
-                'pkg-4@0.1.0',
-                'pkg-5@0.0.2',
-            ])
+        // pkg-2 is explicitly bumped with minor
+        expect(result['pkg-2'].version).toEqual('0.2.0')
+        expect(result['pkg-2'].changelog).toEqual(
+            expect.stringContaining('some other feature'),
+        )
 
-            const filesToCheck = [
-                '.prepack.test.tmp',
-                '.prepare.test.tmp',
-                '.prepublish.test.tmp',
-                '.postpack.test.tmp',
-                '.postpublish.test.tmp',
-            ]
+        // pkg-5 depends on pkg-4, so it'll be bumped as a dependant
+        // pkg-3 is bumped because it depends on pkg-2
+        // pkg-6 is bumped because it depends on pkg-3
+        expect(mockGit._getPushedTags_()).toEqual([
+            'pkg-2@0.2.0',
+            'pkg-3@0.0.2',
+            'pkg-4@0.1.0',
+            'pkg-5@0.0.2',
+            'pkg-6@0.0.2',
+        ])
 
-            for (const fileToCheck of filesToCheck) {
-                const filename = resolvePackagePath('pkg-4', fileToCheck)
-                const stat = await fs.stat(filename)
-                expect(stat).toBeDefined()
-            }
-        } finally {
-            // cleanup lifecycle artifacts
-            await cleanup('pkg-4')
+        const filesToCheck = [
+            '.prepack.test.tmp',
+            '.prepare.test.tmp',
+            '.prepublish.test.tmp',
+            '.postpack.test.tmp',
+            '.postpublish.test.tmp',
+        ]
+
+        for (const fileToCheck of filesToCheck) {
+            const filename = resolvePackagePath('pkg-4', fileToCheck)
+            const stat = await fs.stat(filename)
+            expect(stat).toBeDefined()
         }
     })
 })
