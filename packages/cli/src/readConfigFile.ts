456c71a81e242c2922a9b2115b3a0b74f1df0467
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2021-04-07 13:48:16 -0400

    feat(cli): add configuration file support, close #281

---
+++ packages/cli/src/readConfigFile.ts
@@ -0,0 +1,24 @@
+import { ConfigFile } from './types'
+
+const readConfigFile = async (
+    configFilename: string,
+    { cwd }: { cwd: string },
+): Promise<ConfigFile> => {
+    try {
+        const configId = require.resolve(configFilename, { paths: [cwd] })
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const config = require(configId) as ConfigFile
+        return config
+    } catch (err) {
+        if (err?.message) {
+            throw new Error(
+                `Unable to parse monodeploy config from: ${configFilename}.\n\n${err.message}`,
+            )
+        }
+
+        /* istanbul ignore next: this just surfaces the original error */
+        throw err
+    }
+}
+
+export default readConfigFile
