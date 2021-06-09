8738bb9a40ee66712fd8da68f345bdb60adfccb7
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2021-06-09 09:41:51 -0400

    chore: simplify test run

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 const CI = process.env.CI === '1'
 const ARTIFACT_DIR = process.env.ARTIFACT_DIR || 'artifacts'
+const IS_E2E = process.env.E2E === '1'
 
 module.exports = {
     ...(CI && {
@@ -19,7 +20,7 @@ module.exports = {
     transform: {
         '^.+\\.[jt]sx?$': 'ts-jest',
     },
-    coverageReporters: CI ? ['json'] : ['text-summary', 'json'],
+    coverageReporters: CI ? ['json'] : ['text', 'json'],
     coverageDirectory: `raw-coverage/jest/`,
     collectCoverageFrom: [
         'packages/**/src/**/*.ts',
@@ -37,6 +38,7 @@ module.exports = {
     testPathIgnorePatterns: [
         '<rootDir>/.*\\.js',
         '<rootDir>/.*/lib/',
+        ...(IS_E2E ? ['<rootDir>/packages'] : ['<rootDir>/e2e-tests']),
     ],
     haste: {
         throwOnModuleCollision: true,
