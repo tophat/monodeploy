06ea79f5459dec019fbacadf5c2d7692a8381afc
Noah <noah.negin-ulster@tophatmonocle.com>
2022-01-01 13:05:02 -0500

    fix: use per-scope fetch registry from .yarnrc.yml, #457 (#458)

--- packages/versions/src/getRegistryUrl.ts
+++ packages/versions/src/getRegistryUrl.ts
@@ -1,12 +1,15 @@
 import type { MonodeployConfiguration, YarnContext } from '@monodeploy/types'
+import { Workspace } from '@yarnpkg/core'
 import * as pluginNPM from '@yarnpkg/plugin-npm'
 
 export const getFetchRegistryUrl = async ({
     config,
     context,
+    workspace,
 }: {
     config: MonodeployConfiguration
     context: YarnContext
+    workspace: Workspace
 }): Promise<string | null> => {
     if (config.noRegistry) {
         return null
@@ -17,7 +20,7 @@ export const getFetchRegistryUrl = async ({
         return pluginNPM.npmConfigUtils.normalizeRegistry(configRegistryUrl)
     }
 
-    return await pluginNPM.npmConfigUtils.getDefaultRegistry({
+    return await pluginNPM.npmConfigUtils.getScopeRegistry(workspace.manifest.name?.scope ?? null, {
         configuration: context.configuration,
         type: pluginNPM.npmConfigUtils.RegistryType.FETCH_REGISTRY,
     })
