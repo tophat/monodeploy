5474d1056ad3715f9c8f16f4b336ead11b4ff332
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2022-01-05 20:33:56 -0500

    fix: fail on corrupt manifest patching
    feat: add progress bars for patching manifest and savepoints

--- packages/io/src/backupPackage.ts
+++ packages/io/src/backupPackage.ts
@@ -2,7 +2,9 @@ import { promises as fs } from 'fs'
 import os from 'os'
 import path from 'path'
 
+import logging from '@monodeploy/logging'
 import type { MonodeployConfiguration, YarnContext } from '@monodeploy/types'
+import { Report } from '@yarnpkg/core'
 
 import getPackageJsonPaths from './getPackageJsonPaths'
 
@@ -26,16 +28,48 @@ export const backupPackageJsons = async ({
     return tmpDir
 }
 
-export const restorePackageJsons = async ({ key }: { key: string }): Promise<void> => {
+export const restorePackageJsons = async ({
+    report,
+    key,
+}: {
+    report: Report
+    key: string
+}): Promise<void> => {
+    logging.debug(`[Savepoint] Restoring modified working tree (key: ${key})`, { report })
+
     const map = JSON.parse(await fs.readFile(path.join(key, 'map.json'), 'utf-8'))
+    const entries = Object.entries(map) as [string, string][]
+
+    const progress = Report.progressViaCounter(entries.length)
+    report.reportProgress(progress)
+
+    const restoreFile = async (index: string, filename: string): Promise<void> => {
+        const src = path.join(key, index)
+        await fs.copyFile(src, filename)
+    }
+
     await Promise.all(
-        (Object.entries(map) as [string, string][]).map(async ([index, filename]) => {
-            const src = path.join(key, index)
-            await fs.copyFile(src, filename)
-        }),
+        entries.map(([index, filename]) =>
+            restoreFile(index, filename).finally(() => progress.tick()),
+        ),
     )
 }
 
-export const clearBackupCache = async ({ keys }: { keys: string[] }): Promise<void> => {
-    await Promise.all(keys.map(async (key) => await fs.rm(key, { recursive: true, force: true })))
+export const clearBackupCache = async ({
+    report,
+    keys,
+}: {
+    report: Report
+    keys: string[]
+}): Promise<void> => {
+    logging.debug('[Savepoint] Deleting temporary savepoint files', { report })
+
+    const progress = Report.progressViaCounter(keys.length)
+    report.reportProgress(progress)
+
+    await Promise.all(
+        keys.map((key) =>
+            fs.rm(key, { recursive: true, force: true }).finally(() => progress.tick()),
+        ),
+    )
 }
