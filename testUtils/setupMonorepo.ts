6aebff9b2b9c55c93ff061baa41d7a21169b8974
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2022-10-11 12:18:00 -0400

    fix: update to yarn 3.2.4

--- testUtils/setupMonorepo.ts
+++ testUtils/setupMonorepo.ts
@@ -75,7 +75,7 @@ export default async function setupMonorepo(
             useRelativePath: false,
         }),
         repository: root?.repository,
-        packageManager: 'yarn@3.2.3',
+        packageManager: 'yarn@3.2.4',
     })
 
     // Generate children workspaces
